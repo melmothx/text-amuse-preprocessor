ad5c73e8232e672e75b940c63f77f43f1ba37665
Marco Pessotto <melmothx@gmail.com>
2014-04-13 19:50:15 +0200

    Fixed manifest

--- lib/Text/Amuse/Preprocessor.pm
+++ lib/Text/Amuse/Preprocessor.pm
@@ -10,11 +10,11 @@ Text::Amuse::Preprocessor - Helpers for Text::Amuse document formatting.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
