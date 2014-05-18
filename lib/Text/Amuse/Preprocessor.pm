6cc363cd7d97e751b0d9f6a570437416ac88bd59
Marco Pessotto <melmothx@gmail.com>
2014-05-18 11:58:30 +0200

    Bumped version (0.09)

--- lib/Text/Amuse/Preprocessor.pm
+++ lib/Text/Amuse/Preprocessor.pm
@@ -10,11 +10,11 @@ Text::Amuse::Preprocessor - Helpers for Text::Amuse document formatting.
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 
 =head1 SYNOPSIS
