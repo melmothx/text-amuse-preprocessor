8bb8cc99b203b2447b72b3c8700e90bdc40bfba2
Marco Pessotto <melmothx@gmail.com>
2014-10-12 12:18:12 +0200

    Tests and error handling in footnotes rearranging

--- lib/Text/Amuse/Preprocessor/Footnotes.pm
+++ lib/Text/Amuse/Preprocessor/Footnotes.pm
@@ -7,6 +7,108 @@ use File::Temp;
 use File::Copy;
 use Data::Dumper;
 
+=encoding utf8
+
+=head1 NAME
+
+Text::Amuse::Preprocessor::Footnotes - Rearrange footnote numbering in muse docs
+
+=head1 DESCRIPTION
+
+Given an input file, scan its footnotes and rearrange them. This means
+that such document:
+
+  #title test
+  
+  Hello [1] There [1] Test [1]
+  
+  [1] first
+  
+  Hello hello
+
+  [1] second
+  
+  [1] third
+
+will become
+
+  #title test
+  
+  Hello [1] There [2] Test [3]
+  
+  Hello hello
+
+  [1] first
+  
+  [2] second
+  
+  [3] third
+
+Given that the effects of the rearranging could be very destructive
+and mess up your documents, the module try to play on the safe side
+and will refuse to write out a file if there is a count mismatch
+between the footnotes and the number of references in the body.
+
+The core concept is that the module doesn't care about the number.
+Only order matters.
+
+This could be tricky if the document uses the number between square
+brackets for other than footnotes.
+
+Also used internally by L<Text::Amuse::Preprocessor>.
+
+=head1 METHODS
+
+=head2 new(input => $infile, output => $outfile, debug => 0);
+
+Constructor with the following options:
+
+=head3 input
+
+The input file. It must exists.
+
+=head3 output
+
+The output file. It will be written by the module if the parsing
+succeedes.
+
+=head3 debug
+
+Print some additional info.
+
+=head2 process
+
+Do the job, write out C<output> and return C<output>. On failure, set
+an arror and return false.
+
+=head2 error
+
+Accesso to the error. If there is a error, an hashref with the
+following keys will be returned:
+
+=over 4
+
+=item reference
+
+The total number of footnote references in the body.
+
+=item footnotes
+
+The total number of footnotes.
+
+=item references_found
+
+The reference's numbers found in the body as a long string.
+
+=item footnotes_found
+
+The footnote' numbers found in the body as a long string.
+
+=back
+
+=cut
+
+
 sub new {
     my ($class, %options) = @_;
     my $self = {
@@ -69,6 +171,14 @@ sub tmpdir {
     return $self->{_tmpdir}->dirname;
 }
 
+sub _fn_re {
+    return qr/^\[[0-9]{1,4}\](?=\s)/;
+}
+
+sub _ref_re {
+    return qr/\[[0-9]{1,4}\]/;
+}
+
 sub process {
     my $self = shift;
     print Dumper($self) if $self->debug;
@@ -90,9 +200,11 @@ sub process {
     my $body_fn_counter = 0;
     my $last_was_fn = undef;
 
+    my $fn_re = $self->_fn_re;
+    my $ref_re = $self->_ref_re;
     while (my $r = <$in>) {
         # a footnote
-        if ($r =~ s/^\[\d+\](?=\s)/'[' . ++$fn_counter . ']'/e) {
+        if ($r =~ s/$fn_re/'[' . ++$fn_counter . ']'/e) {
             # the footnotes at the end go in a separate array
             print $fn_fh $r;
             $last_was_fn = $r;
@@ -113,7 +225,7 @@ sub process {
         }
 
         # then process the page
-        $r =~ s/\[\d{1,4}\]/"[" . ++$body_fn_counter . "]"/ge;
+        $r =~ s/$ref_re/'[' . ++$body_fn_counter . ']'/ge;
         print $body_fh $r;
     }
 
@@ -142,12 +254,38 @@ sub process {
         return $self->output;
     }
     else {
-        $self->_set_error({
-                           references => $body_fn_counter,
-                           footnotes  => $fn_counter,
-                          });
+        $self->_report_error($body_fn_counter, $fn_counter);
         return;
     }
 }
+
+sub _report_error {
+    my ($self, $body_fn_counter, $fn_counter) = @_;
+    my $fn_re = $self->_fn_re;
+    my $ref_re = $self->_ref_re;
+    # reopen and rescan
+    my $in_file = $self->input;
+    open (my $in, '<:encoding(UTF-8)', $in_file) or die ("$in_file $!");
+    my @footnotes;
+    my @references;
+    while (my $line = <$in>) {
+        if ($line =~ m/$fn_re/) {
+            push @footnotes, $&;
+            next;
+        }
+        while ($line =~ m/$ref_re/g) {
+            push @references, $&;
+        }
+    }
+    close $in;
+    $self->_set_error({
+                       reference => $body_fn_counter,
+                       footnotes => $fn_counter,
+                       references_found => join(" ", @references),
+                       footnotes_found  => join(" ", @footnotes),
+                      });
+
+}
+
 
 1;
--- t/footnotes.t
+++ t/footnotes.t
@@ -8,7 +8,7 @@ use File::Spec::Functions qw/catfile catdir/;
 use Text::Amuse::Preprocessor;
 use Text::Amuse::Preprocessor::Footnotes;
 
-use Test::More tests => 2;
+use Test::More tests => 9;
 use Data::Dumper;
 
 my $good = catfile(qw/t footnotes good.muse/);
@@ -20,7 +20,39 @@ my $pp = Text::Amuse::Preprocessor::Footnotes->new(input => $good,
                                                   );
 
 ok ($pp->process, "success") or diag Dumper($pp->error);
+ok (!$pp->error);
 compare_files($out, $expected);
+
+my $too_many_refs = catfile(qw/t footnotes bad.muse/);
+
+$pp = Text::Amuse::Preprocessor::Footnotes->new(output => $out,
+                                                input => $too_many_refs,
+                                                debug  => 1,
+                                               );
+ok (!$pp->process, "No success");
+ok (! -f $out, "$out not written");
+is_deeply ($pp->error, {
+                        reference => 3,
+                        footnotes => 2,
+                        references_found => '[1] [2] [4]',
+                        footnotes_found  => '[1] [1]',
+                       }, "Error found");
+
+my $too_many_fns = catfile(qw/t footnotes bad2.muse/);
+
+$pp = Text::Amuse::Preprocessor::Footnotes->new(output => $out,
+                                                input => $too_many_fns,
+                                                debug  => 1,
+                                               );
+ok (!$pp->process, "No success");
+ok (! -f $out, "$out not written");
+is_deeply ($pp->error, {
+                        reference => 3,
+                        footnotes => 4,
+                        references_found => '[1] [2] [4]',
+                        footnotes_found  => '[1] [1] [4] [5]',
+                       }, "Error found");
+
 
 sub compare_files {
     my ($got, $exp) = @_;
