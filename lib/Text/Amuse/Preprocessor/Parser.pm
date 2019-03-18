8c1dc41a67a850d2c534a951b61f37bca3cacadf
Marco Pessotto <melmothx@gmail.com>
2019-03-18 08:29:43 +0100

    Manifest and pod

--- lib/Text/Amuse/Preprocessor/Parser.pm
+++ lib/Text/Amuse/Preprocessor/Parser.pm
@@ -4,6 +4,32 @@ use utf8;
 use strict;
 use warnings;
 
+=head1 NAME
+
+Text::Amuse::Preprocessor::Parser - Stripped down Muse parser for Text::Amuse::Preprocessor
+
+=head2 FUNCTIONS
+
+=over 4
+
+=item parse_text($body)
+
+Parse the string provided as argument and return a list of hashrefs
+with this structure:
+
+  {
+   type => "markup" || "text",
+   string => $chunk
+  }
+
+the concatenation of the C<string> values is equal to the original
+body (without carriage returns and null bytes, tabs normalized and
+final newline appended if missing).
+
+=back
+
+=cut
+
 sub parse_text {
     my $string = shift;
     $string =~ s/[\r\0]//g;
